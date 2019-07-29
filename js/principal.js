4056161c3bd559ea5bf21418f7c10aeff41ee8ff
DESKTOP-1JNEHFN\Crummorn <igorcaze@gmail.com>
2019-07-29 14:33:30 -0300

    Arrays, Loop e Estilos

--- js/principal.js
+++ js/principal.js
@@ -1,9 +1,9 @@
 var titulo = document.querySelector(".js-titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-for (let index = 0; index < 5; index++) {
-    var paciente = document.querySelector(".js-paciente-" + (index + 1));
-    
+var pacientes = document.querySelectorAll("#js-tabela-pacientes .js-paciente")
+
+pacientes.forEach(paciente => {
     var pacientePeso = paciente.querySelector('.js-info-peso').textContent;
     var pacienteAltura = paciente.querySelector('.js-info-altura').textContent;
     var pacienteIMC = paciente.querySelector(".js-info-imc");
@@ -14,17 +14,19 @@ for (let index = 0; index < 5; index++) {
     if (pacientePeso <= 0 || pacientePeso >= 1000) {
         validaPeso = false;
         pacienteIMC.textContent = "Peso Inválido!";
+        paciente.classList.add('paciente-invalido');
     }
     
     if (pacienteAltura <= 0 || pacienteAltura >= 3.00) {
         validaAltura = false;
         pacienteIMC.textContent = "Altura Inválida!";
+        paciente.classList.add('paciente-invalido');
     }
     
     if (validaPeso && validaAltura) {
         var imcCalculado = pacientePeso / (pacienteAltura * pacienteAltura);
-        pacienteIMC.textContent = imcCalculado;
+        pacienteIMC.textContent = imcCalculado.toFixed(2); // ToFixed formata as casas decimais
     }
-}
+});
 
 
