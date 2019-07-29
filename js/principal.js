834cc4f2d0150f85a50aaf8cf93c6d93789476a4
DESKTOP-1JNEHFN\Crummorn <igorcaze@gmail.com>
2019-07-29 15:09:29 -0300

    Eventos, Formulários e Criando Paciente

--- js/principal.js
+++ js/principal.js
@@ -1,8 +1,9 @@
+// Edição de Titulo
 var titulo = document.querySelector(".js-titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
+// Calculo do IMC
 var pacientes = document.querySelectorAll("#js-tabela-pacientes .js-paciente")
-
 pacientes.forEach(paciente => {
     var pacientePeso = paciente.querySelector('.js-info-peso').textContent;
     var pacienteAltura = paciente.querySelector('.js-info-altura').textContent;
@@ -29,4 +30,46 @@ pacientes.forEach(paciente => {
     }
 });
 
+// Formulario de Adição
+var botaoAdd = document.querySelector("#adicionar-paciente");
 
+botaoAdd.addEventListener('click', adicionarPaciente);
+
+function adicionarPaciente () {
+    event.preventDefault();
+
+    var form = document.querySelector(".js-form-adiciona");
+
+    var pacienteNome = form.nome.value;
+    var pacientePeso = form.peso.value;
+    var pacienteAltura = form.altura.value;
+    var pacienteGordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add('paciente');
+    var nomeTd = document.createElement("td");
+    nomeTd.classList.add('js-info-nome');
+    var pesoTd = document.createElement("td");
+    pesoTd.classList.add('js-info-peso');
+    var alturaTd = document.createElement("td");
+    alturaTd.classList.add('js-info-altura');
+    var gorduraTd = document.createElement("td");
+    gorduraTd.classList.add('js-info-gordura');
+    var imcTd = document.createElement("td");
+    imcTd.classList.add('js-info-imc');
+
+    nomeTd.textContent = pacienteNome;
+    pesoTd.textContent = pacientePeso;
+    alturaTd.textContent = pacienteAltura;
+    gorduraTd.textContent = pacienteGordura;
+    imcTd.textContent = 0;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
+
+   var tabelaPacientes = document.querySelector('#js-tabela-pacientes');
+   tabelaPacientes.appendChild(pacienteTr);
+}
