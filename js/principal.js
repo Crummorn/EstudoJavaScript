49f6cb0e59801248638524a5330aca95122b3376
DESKTOP-1JNEHFN\Crummorn <igorcaze@gmail.com>
2019-07-29 14:09:12 -0300

    Variáveis e Operadores JS

--- js/principal.js
+++ js/principal.js
@@ -2,14 +2,29 @@ var titulo = document.querySelector(".js-titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
 for (let index = 0; index < 5; index++) {
-    var paciente = document.querySelector(".js-paciente-" + (index + 1) )
+    var paciente = document.querySelector(".js-paciente-" + (index + 1));
     
-    var pacientePeso = paciente.querySelector('.js-info-peso').textContent
-    var pacienteAltura = paciente.querySelector('.js-info-altura').textContent
+    var pacientePeso = paciente.querySelector('.js-info-peso').textContent;
+    var pacienteAltura = paciente.querySelector('.js-info-altura').textContent;
+    var pacienteIMC = paciente.querySelector(".js-info-imc");
     
-    var pacienteIMC = pacientePeso / (pacienteAltura * pacienteAltura)
+    var validaPeso = true;
+    var validaAltura = true;
     
-    paciente.querySelector(".js-info-imc").textContent = pacienteIMC;
+    if (pacientePeso <= 0 || pacientePeso >= 1000) {
+        validaPeso = false;
+        pacienteIMC.textContent = "Peso Inválido!";
+    }
+    
+    if (pacienteAltura <= 0 || pacienteAltura >= 3.00) {
+        validaAltura = false;
+        pacienteIMC.textContent = "Altura Inválida!";
+    }
+    
+    if (validaPeso && validaAltura) {
+        var imcCalculado = pacientePeso / (pacienteAltura * pacienteAltura);
+        pacienteIMC.textContent = imcCalculado;
+    }
 }
 
 
